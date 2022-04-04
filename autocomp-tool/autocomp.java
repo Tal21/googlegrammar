e1ebea07585000ed52ec3ad0cddec5c4b579cd8c
Lauren Lee <llee30@stuy.edu>
2022-04-04 16:22:08 -0400

    continuous autocomplete

--- autocomp-tool/autocomp.java
+++ autocomp-tool/autocomp.java
@@ -16,31 +16,91 @@ public static void main(String[] args) throws Exception {
   System.out.println(data);
   */
 
-  System.out.println("input a sentence for autocompletion");
-  Scanner in = new Scanner(System.in);
-  String strIn = in.nextLine();
+    System.out.println("input a sentence for autocompletion or input exit to exit");
+    Scanner in = new Scanner(System.in);
+    String strIn = in.nextLine();
+    String url = "https://api-inference.huggingface.co/models/gpt2";  // example url which return json data
+    String data = readUrl(url, strIn);
+    System.out.println(createSentenceToken(strIn, data));
+    while (strIn != "exit") {
+    //  Scanner in = new Scanner(System.in);
+      strIn = in.nextLine();
+      // System.out.println("input a sentence for autocompletion or input exit to exit");
+      url = "https://api-inference.huggingface.co/models/gpt2";  // example url which return json data
+      data = readUrl(url, strIn);
+      System.out.println(createSentenceToken(strIn, data));
+    }
+    in.close();
+    // StringTokenizer tokendata = new StringTokenizer(data);
+    // StringTokenizer intok = new StringTokenizer(strIn);
+    // int incount = 0;
+    // while(intok.hasMoreTokens()){
+    //   String h = intok.nextToken();
+    //   incount++;
+    // }
+    //
+    // String printtoken = "";
+    // int i = 0;
+    // while(i < incount + 1){
+    //   printtoken = printtoken+  " " + tokendata.nextToken();
+    //   i++;
+    // }
 
-  String url = "https://api-inference.huggingface.co/models/gpt2";  // example url which return json data
-  String data = readUrl(url, strIn);
-  StringTokenizer tokendata = new StringTokenizer(data);
-  StringTokenizer intok = new StringTokenizer(strIn);
-  int incount = 0;
-  while(intok.hasMoreTokens()){
-    String h = intok.nextToken();
-    incount++;
-  }
+    //JSONObject json = new JSONObject(data);
+    //System.out.println(createSentenceToken(strIn, data));
 
-  String printtoken = "";
-  int i = 0;
-  while(i < incount + 1){
-    printtoken = printtoken+  " " + tokendata.nextToken();
-    i++;
-  }
-
-  //JSONObject json = new JSONObject(data);
-  System.out.println(printtoken);
 }
 
+//hello returns a link
+public static String createOneToken(String input, String data){
+  StringTokenizer tokendata = new StringTokenizer(data);
+  StringTokenizer intok = new StringTokenizer(input);
+  /*
+  int incount = 0;
+  while(intok.hasMoreTokens()){
+    String h = intok.nextToken(); //what is h for?
+    incount++;
+  }
+  */
+
+  String printtoken = "";
+  for(int i = 0; i <= intok.countTokens(); i++){
+    printtoken = printtoken+  " " + tokendata.nextToken();
+  }
+  /*
+  String nex = tokendata.nextToken();
+  while (!nex.equals(",")||!nex.equals(".")||!nex.equals(";")||tokendata.hasMoreTokens()){
+      printtoken = printtoken+  " " + nex;
+  }
+  */
+  return printtoken;
+}
+
+public static String createSentenceToken(String input, String data){
+  StringTokenizer tokendata = new StringTokenizer(data);
+  StringTokenizer intok = new StringTokenizer(input);
+  /*
+  int incount = 0;
+  while(intok.hasMoreTokens()){
+    String h = intok.nextToken(); //what is h for?
+    incount++;
+  }
+  */
+
+  String printtoken = "";
+  for(int i = 0; i <= intok.countTokens(); i++){
+    printtoken = printtoken+  " " + tokendata.nextToken();
+  }
+
+  String nex = tokendata.nextToken();
+  while (nex.indexOf(".") < 0 && nex.indexOf("!") < 0 && nex.indexOf("?") < 0){
+    nex = tokendata.nextToken();
+    //System.out.println(nex);
+    printtoken = printtoken +  " " + nex;
+  }
+
+  return printtoken;
+}
 
 
 private static String readUrl(String urlString, String postData) throws Exception {
