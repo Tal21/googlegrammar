33e094c3c44fd781bfbc81fbc67d1824d775056f
Diana Akhmedova <dakhmedova30@stuy.edu>
2022-05-11 11:50:22 -0400

    updated spellchecker

--- autocomp-tool/spellchecker.java
+++ autocomp-tool/spellchecker.java
@@ -43,24 +43,42 @@ public class spellchecker {
         errorCount++;
         //errors = true;
 
-        System.out.println("\nWould you like to add " + check + " to the dictionary? (yes/no)");
+        String[] threeWords = similarWord(input, dictionary);
+        System.out.println("Did you mean: " + threeWords[0] + "[1], " + threeWords[1] + "[2], or " + threeWords[2] + "[3]? (no/1/2/3)");
         Scanner in = new Scanner(System.in);
         String strIn = in.nextLine();
-
-        if (strIn.toLowerCase().equals("yes") || strIn.toLowerCase().equals("y")) {
-          try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("words.txt", true));
-            writer.write(check);
-            writer.close();
-            errorCount--;
-          }
-          catch (IOException e) {
-            System.out.println(e);
-          }
+        if (strIn.equals("1")) {
+          check = threeWords[0];
+          errorCount--;
+        }
+        else if (strIn.equals("2")) {
+          check = threeWords[1];
+          errorCount--;
+        }
+        else if (strIn.equals("3")) {
+          check = threeWords[2];
+          errorCount--;
         }
         else {
-          System.out.println("\nThe word was not added.");
-          //put in closest word
+          System.out.println("\nWould you like to add " + check + " to the dictionary? (yes/no)");
+          Scanner in = new Scanner(System.in);
+          String strIn = in.nextLine();
+
+          if (strIn.toLowerCase().equals("yes") || strIn.toLowerCase().equals("y")) {
+            try {
+              BufferedWriter writer = new BufferedWriter(new FileWriter("words.txt", true));
+              writer.write(check);
+              writer.close();
+              errorCount--;
+            }
+            catch (IOException e) {
+              System.out.println(e);
+            }
+          }
+          else {
+            System.out.println("\nThe word was not added.");
+            //put in closest word
+          }
         }
       }
     }
@@ -112,7 +130,51 @@ public class spellchecker {
       if letter difference count is greater than carrying variable replace.
 
     */
+    return "";
+  }
 
+  public static String[] similarWord(String word, String[] dictionary) {
+    ArrayList<String> similarWords = new ArrayList<String>();
+    String[] threeWords = new String[3];
+
+    for (String s : dictionary) {
+      if (s.length() == word.length()) {
+        int difference = checkDifference(s, word);
+        if (difference <= 3) {
+          if (similarWords.isEmpty()) {
+            similarWords.add(s);
+          }
+          else {
+            for (int i = 0; i <= similarWords.size(); i++) {
+              int diffAtI = checkDifference(similarWords.get(i), word);
+              if (difference <= diffAtI) {
+                similarWords.add(i, s);
+              }
+            }
+          } // end else
+        } // end second if
+      } // end first if
+    } // end main for loop
+    for (int i = 0; i < threeWords.length; i++) {
+      threeWords[i] = similarWords.get(i);
+    }
+    return threeWords;
+  } // end method
+
+  public static int checkDifference(String s, String word) {
+    int difference = 0;
+
+    for (int i = 0; i < word.length(); i++) {
+      if (s.charAt(i) != word.charAt(i)) {
+        difference++;
+      }
+    }
+    return difference;
+  }
+
+    // List<String> words = myList.stream()
+    //                      .filter(s -> s.contains(word)
+    //                      .collect(Collectors.toList()));
   }
 
 }
